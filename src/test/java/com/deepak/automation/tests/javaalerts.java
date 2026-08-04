cc8b7db8090db7891b53bc5e076215b00578834b
Deepak Murugesan <deepakmurugesan01@gmail.com>
2026-08-04 16:26:38 +0530

    Test

---
+++ src/test/java/com/deepak/automation/tests/javaalerts.java
@@ -0,0 +1,38 @@
+package com.deepak.automation.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.By;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class javaalerts {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void Setup() {
+        driver = new FirefoxDriver();
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(java.util.Objects.requireNonNull(java.time.Duration.ofSeconds(5)));
+    }
+
+    @Test
+    public void testAlerts() {
+        String name = "Deepak";
+        driver.findElement(By.id("name")).sendKeys(name);
+        driver.findElement(By.id("alertbtn")).click();
+        System.out.println(driver.switchTo().alert().getText());
+        driver.switchTo().alert().accept();
+
+        driver.findElement(By.id("confirmbtn")).click();
+        System.out.println(driver.switchTo().alert().getText());
+        driver.switchTo().alert().dismiss();
+
+        
+       
+      
+    }
+
+    
+}
